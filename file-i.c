dadcd34bda7c172e57c5b92e2c058980d1c16787
ji won <165463130+dlwleasy@users.noreply.github.com>
2025-04-20 23:26:13 +0900

    Update file-i.c

--- file-i.c
+++ file-i.c
@@ -16,11 +16,16 @@ int main(void)
 
 	printf("file descritor: %d\n",fd);
 
-	write(fd, buf, sizeof(buf));
+	write(fd, buf, sizeof(buf)); /char buf[]="Computer Network 2025 ZZANG!\n"; 버퍼내용 fd에 write
 
-	fd1=open("read_data_file.txt",O_RDONLY);
+	fd1=open("read_data_file.txt",O_RDONLY);   /값은 1씩증가,3부터 증가? -> 처음 디스크립터 3 
+
+		/ 0,1,2, : 각각 존재함
+		/ 0 : 키보드 . 표준입력 ,1 : 모니터 . 표준출력, 2 표준에러
 
 	read_len=read(fd1,read_buf,sizeof(read_buf));
+	
+	/읽어줌, ("read_data_file.txt") 이 내용
 
 	printf("Read result: %s",read_buf);
 
